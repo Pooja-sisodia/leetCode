ff2c615fe4ac935d53c3b02631d77ee26f276867
Pooja-sisodia <115547318+Pooja-sisodia@users.noreply.github.com>
2023-02-04 03:31:53 +0530

    Time: 93 ms (35.72%), Space: 48.2 MB (58.94%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -2,9 +2,26 @@
  * @param {string} s
  * @return {string}
  */
-var reverseWords = function(s) {
-    return s.split(" ").map(function(word) {
-        return word.split("").reverse().join("");
-    }).join(" ");
-
+ var reverseWords = function(s) {
+    // return s.split(" ").map(function(word) {
+    //     return word.split("").reverse().join("");
+    // }).join(" ");
+     
+     let str = s.split(" ")
+     let arr = [];
+     for(let i=0; i<str.length; i++){
+         let Estr = str[i].split("")
+         let l = 0; 
+         let h =  Estr.length-1;
+         while(l<h){
+             Estr[l] = Estr[l].concat(Estr[h])
+             Estr[h]= Estr[l].slice(0,-1)
+             Estr[l] = Estr[l].slice(-1)
+               l++;
+               h--;
+         }
+          arr.push(Estr.join(""))
+          
+     }
+      return arr.join(" ")
 };
